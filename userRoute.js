d62ad5a6be851ecf0571485df8da1e1b9aa070f7
cngvnn <chinguunadiya3@gmail.com>
2024-12-05 18:18:10 +0800

    Add files via upload

---
+++ userRoute.js
@@ -0,0 +1,32 @@
+const Route = require("express");
+const userRoute = Route();
+const userModel = require("../module/userSchema");
+const bcrypt = require("bcrypt");
+userRoute.post("/signup", async (req, res) => {
+  try {
+    const { password } = req.body;
+    if (password) {
+      const hash = await bcrypt.hashSync(password, 10);
+      req.body.password = hash;
+    }
+    const user = req.body;
+    const response = await userModel.create(user);
+    console.log(response);
+    res.send("User created successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("Error");
+  }
+});
+userRoute.get("/user/posts", async (req, res) => {
+  try {
+    const respo = await userModel
+      .find()
+      .populate("post", "postImg description");
+    res.send(respo);
+  } catch (error) {
+    res.status(403).send(error);
+    console.log("errrrrrrooorrr");
+  }
+});
+module.exports = userRoute;
